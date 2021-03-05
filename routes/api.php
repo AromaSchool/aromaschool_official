b477f368b8686c2326131a0c4badc1d3ca3a8e78
Paul <alphagoo@g-mail.nsysu.edu.tw>
2021-03-05 22:00:10 +0800

    add ping pong test api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('ping', [Controllers\PingController::class, 'ping']);
