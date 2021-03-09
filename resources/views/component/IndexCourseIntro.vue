78a6fb674f46b841d3e6b37856ef61a0e950eefe
jcsamoyed <aruby5223@gmail.com>
2021-03-09 15:35:55 +0800

    make index components

---
+++ resources/views/component/IndexCourseIntro.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="course_intro_card wow animated fadeInUp">
+    <div class="container">
+      <div class="course_intro_block">
+        <div class="course_intro_box">
+          <div class="course_content">
+            <figure>
+              <img
+                src="../../image/index/course_icon-1.svg"
+                alt="芳香療法認證課程入門班"
+              />
+            </figure>
+            <h4><span>NAHA Level 2</span>芳香療法認證課程入門班</h4>
+            <p>
+              從這裡開始建立芳療的基礎觀念及基本運用方法，感受精油的自然療癒力量，引領您進入芳療的世界。
+            </p>
+          </div>
+          <a href="#" class="btn_underline brown" title="深入了解">深入了解</a>
+        </div>
+        <div class="course_intro_box">
+          <div class="course_content">
+            <figure>
+              <img
+                src="../../image/index/course_icon-2.svg"
+                alt="芳香療法認證課程中階班"
+              />
+            </figure>
+            <h4><span>NAHA Level 2</span>芳香療法認證課程中階班</h4>
+            <p>
+              進階瞭解植物藥理及生理機轉，調香學豐富的氣味觸動，香氣抓周的信息導引，有助於了解自己的特質，並能給予個案客觀的分析。
+            </p>
+          </div>
+          <a href="#" class="btn_underline brown" title="深入了解">深入了解</a>
+        </div>
+        <div class="course_intro_box">
+          <div class="course_content">
+            <figure>
+              <img
+                src="../../image/index/course_icon-3.svg"
+                alt="芳香療法認證課程高階班"
+              />
+            </figure>
+            <h4><span>NAHA Level 2</span>芳香療法認證課程高階班</h4>
+            <p>
+              打下基礎後，高階班是為晉升專業芳療師打造的課程，120小時全方位紮實的學習成為芳療師所需具備的專業科目。
+            </p>
+          </div>
+          <a href="#" class="btn_underline brown" title="深入了解">深入了解</a>
+        </div>
+        <div class="course_intro_box">
+          <div class="course_content">
+            <figure>
+              <img
+                src="../../image/index/course_icon-4.svg"
+                alt="臨床芳療師認證課程"
+              />
+            </figure>
+            <h4><span>NAHA Level 3</span>臨床芳療師認證課程</h4>
+            <p>
+              芳療臨床訓練強調透過醫學、護理、生化的角度解析，更近一步瞭解精油的科學分析與我們人體互動的實證效果。
+            </p>
+          </div>
+          <a href="#" class="btn_underline brown" title="深入了解">深入了解</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
