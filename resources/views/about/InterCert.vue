183d5eba4df074e61d31a202e09ec1998d677b9f
Paul <alphgoo@g-mail.nsysu.edu.tw>
2021-03-15 15:30:18 +0800

    update internal cert

--- resources/views/about/InterCert.vue
+++ resources/views/about/InterCert.vue
@@ -1,7 +1,59 @@
 <template>
   <div class="container blog_container">
     <Sidebar></Sidebar>
-    <ContentCenter></ContentCenter>
+    <ContentCenter :title="title" :date="date">
+      <div>
+        &nbsp;<span style="white-space: pre"> </span
+        >【記者陳玲芳專題報導】「芳療」被現代人視為紓解壓力、放鬆身心、觸動五感的養生保健之道。面對坊間各式各樣、琳瑯滿目的精油、香氛產品，人們也出現許多自以為是、似是而非的使用方式。為免民眾「未蒙其利，先受其害」，專家列舉五大精油迷思，提醒民眾唯有了解，方能安心；「用對了，才有療效」。
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>以精油當媒介</div>
+
+      <div>&nbsp;</div>
+
+      <div><strong>達保健功效</strong></div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        台灣芳療協會資深講師&nbsp;<em>任雲嫻&nbsp;&nbsp;</em>表示，「芳香療法」在歐洲歷史悠久，近年來也成為國人回歸自然的養生方法之一。所謂「芳香療法」，是指藉由芳香植物所萃取出的「精油」為媒介，以按摩、泡澡、薰香等方式，經由呼吸或皮膚吸收進入體內，達到預防身心靈疾病的保健功效。因此「芳療養生」，不啻是一種預防醫學。
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        台灣芳療運用日益廣泛，許多醫院、診所、療養院，也使用這種輔助療法，來幫助人們恢復健康。如淡水馬偕癌症中心，即與民間的芳療師合作，為病友舉辦芳香紓緩講座，並讓社工師引領芳療師到癌症病房，利用精油按摩，為病友紓緩身體不適。除了醫療上的運用，有些公司也開始藉由芳療來照顧員工、紓解工作壓力。
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        <em>任雲嫻&nbsp;</em
+        >列舉坊間常見的五大精油迷思，包括「精油和香精，傻傻分不清」，她強調天然精油是從天然植物中萃取，其成分如同中草藥，不會添加劣質界面活性劑或定香劑；「品質好的精油，可直接抹皮膚」，她表示高濃度的精油塗抹在身上，很容易造成皮膚刺激與灼傷，因此愈純的精油愈需要稀釋，濃度約百分之三左右為宜。
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        「精油愈天然，愈不容易過敏」，其實，過敏跟「天然」與否無關，卻跟個人「體質」有關；「口服精油，更有療效」，她建議避免口服，因為有些精油分子不易被肝臟代謝，恐會造成肝臟負擔；「精油使用頻率，愈多愈有效」，她強調，適時、適量就好，「平衡」才是王道。
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        <br />
+        ■報導原始連結&rarr;&nbsp;<a
+          href="http://www.merit-times.com.tw/epaper.aspx?Unid=%20573206"
+          >http://www.merit-times.com.tw/epaper.aspx?Unid=%20573206</a
+        ><a
+          href="http://www.merit-times.com.tw/epaper.aspx?Unid=%20573206"
+          target="_blank"
+          >&nbsp;</a
+        >
+      </div>
+    </ContentCenter>
   </div>
 </template>
 
@@ -14,5 +66,9 @@ export default {
     Sidebar,
     ContentCenter,
   },
+  data: () => ({
+    title: "人間福報 受邀採訪：「用對才有效 破解精油迷思」",
+    date: "2020/03/26",
+  }),
 };
 </script>
