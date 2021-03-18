f9951287ac9ea38a3cffe310558dbdb035244303
jcsamoyed <aruby5223@gmail.com>
2021-03-18 14:43:00 +0800

    update News page

---
+++ resources/views/component/SearchBox.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="search_box side">
+    <input type="search" placeholder="Search..." class="search_input" />
+    <button type="submit" class="search_btn">
+      <i class="fas fa-search"></i>
+    </button>
+  </div>
+</template>
---
+++ resources/views/news/NewsList.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="container news_list">
+    <div class="index_news">
+      <ul class="news_block">
+        <li>
+          <router-link
+            to="/"
+            class="news_box"
+            title="禾場因應新冠肺炎教室防疫措施升級公告"
+          >
+            <div class="time">
+              <span class="month">03</span>
+              <span class="date">26</span>
+              <span class="year">2020</span>
+            </div>
+            <div class="news_title">
+              <div class="category">課程公告</div>
+              <h5>禾場因應新冠肺炎教室防疫措施升級公告</h5>
+            </div>
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/"
+            class="news_box"
+            title="禾場因應新冠肺炎教室防疫措施升級公告"
+          >
+            <div class="time">
+              <span class="month">03</span>
+              <span class="date">26</span>
+              <span class="year">2020</span>
+            </div>
+            <div class="news_title">
+              <div class="category">課程公告</div>
+              <h5>禾場因應新冠肺炎教室防疫措施升級公告</h5>
+            </div>
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/"
+            class="news_box"
+            title="禾場因應新冠肺炎教室防疫措施升級公告"
+          >
+            <div class="time">
+              <span class="month">03</span>
+              <span class="date">26</span>
+              <span class="year">2020</span>
+            </div>
+            <div class="news_title">
+              <div class="category">課程公告</div>
+              <h5>禾場因應新冠肺炎教室防疫措施升級公告</h5>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
