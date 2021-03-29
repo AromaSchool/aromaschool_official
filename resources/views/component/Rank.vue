c89f47c3c9d05f889bf73870a93ad1d8ec8ac0ae
jcsamoyed <aruby5223@gmail.com>
2021-03-29 17:50:56 +0800

    update Rank css

--- resources/views/component/Rank.vue
+++ resources/views/component/Rank.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="subside_block rank">
-    <h2>公告分類</h2>
+    <h2>最新公告</h2>
     <ol class="subside_box">
       <li>
-        <router-link to="/news/class" title="課程公告">課程公告</router-link>
+        <router-link to="/news/1" title="禾場因應新冠肺炎教室防疫措施升級公告"
+          >禾場因應新冠肺炎教室防疫措施升級公告</router-link
+        >
       </li>
       <li>
-        <router-link to="/news/activity" title="活動公告">活動公告</router-link>
+        <router-link to="/news/1" title="禾場會議中心 － 教室會議室場地租借"
+          >禾場會議中心 － 教室會議室場地租借</router-link
+        >
       </li>
       <li>
-        <router-link to="/news/other" title="其他公告">其他公告</router-link>
+        <router-link
+          to="/news/1"
+          title="Level3臨床中醫芳療，從中醫觀點結合芳療診治身心"
+          >Level3臨床中醫芳療，從中醫觀點結合芳療診治身心</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          to="/news/1"
+          title="看懂植物拉丁學名，成為專業芳師的重要一課！"
+          >看懂植物拉丁學名，成為專業芳師的重要一課！</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/news/1" title="從ACC芳療人格卡學習善待自己也接納別人"
+          >從ACC芳療人格卡學習善待自己也接納別人</router-link
+        >
       </li>
     </ol>
   </div>
