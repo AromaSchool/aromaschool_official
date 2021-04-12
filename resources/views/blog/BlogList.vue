f036c2cd86d69ab9100e6c1b3f1b210cc404cd3d
jcsamoyed <aruby5223@gmail.com>
2021-04-12 14:31:55 +0800

    update BlogList style

--- resources/views/blog/BlogList.vue
+++ resources/views/blog/BlogList.vue
@@ -1,17 +1,144 @@
 <template>
   <section class="container blog_list">
-    <router-link to="/" class="article_box">
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
       <figure>
         <img
           src="@/image/blog/blog_default.svg"
           alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
         />
+        <span class="category">禾場周報</span>
       </figure>
       <div class="article_text">
-        <div class="category">禾場周報</div>
-        <div class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</div>
-        <div class="date">2021/04/09</div>
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
       </div>
+      <div class="article_date">2021/04/09</div>
+    </router-link>
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
+      <figure>
+        <img
+          src="@/image/blog/blog_default.svg"
+          alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+        />
+        <span class="category">禾場周報</span>
+      </figure>
+      <div class="article_text">
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
+      </div>
+      <div class="article_date">2021/04/09</div>
+    </router-link>
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
+      <figure>
+        <img
+          src="@/image/blog/blog_default.svg"
+          alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+        />
+        <span class="category">禾場周報</span>
+      </figure>
+      <div class="article_text">
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
+      </div>
+      <div class="article_date">2021/04/09</div>
+    </router-link>
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
+      <figure>
+        <img
+          src="@/image/blog/blog_default.svg"
+          alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+        />
+        <span class="category">禾場周報</span>
+      </figure>
+      <div class="article_text">
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
+      </div>
+      <div class="article_date">2021/04/09</div>
+    </router-link>
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
+      <figure>
+        <img
+          src="@/image/blog/blog_default.svg"
+          alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+        />
+        <span class="category">禾場周報</span>
+      </figure>
+      <div class="article_text">
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
+      </div>
+      <div class="article_date">2021/04/09</div>
+    </router-link>
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
+      <figure>
+        <img
+          src="@/image/blog/blog_default.svg"
+          alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+        />
+        <span class="category">禾場周報</span>
+      </figure>
+      <div class="article_text">
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
+      </div>
+      <div class="article_date">2021/04/09</div>
+    </router-link>
+    <router-link
+      to="/"
+      class="article_box"
+      title="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+    >
+      <figure>
+        <img
+          src="@/image/blog/blog_default.svg"
+          alt="植物解讀身體的訊息，善用香氣來癒療自己的情緒"
+        />
+        <span class="category">禾場周報</span>
+      </figure>
+      <div class="article_text">
+        <h2 class="title">植物解讀身體的訊息，善用香氣來癒療自己的情緒</h2>
+        <div class="preview">
+          學習芳療之後，我開始會去觀察身邊的親友們，聞到精油後所出現的反應。這些反應所產生的情緒是很值得去探索並深入了解的。
+        </div>
+      </div>
+      <div class="article_date">2021/04/09</div>
     </router-link>
   </section>
 </template>
