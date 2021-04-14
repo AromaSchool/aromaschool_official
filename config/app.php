73f486e719ac104170804d16ed2677db527066c2
Paul <alphagoo@g-mail.nsysu.edu.tw>
2021-04-14 10:12:17 +0800

    register ide helper only in local mode

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Package Service Providers...
