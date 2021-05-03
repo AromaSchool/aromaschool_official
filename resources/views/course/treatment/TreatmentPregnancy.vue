89deccde49fbfc0b7862b6a2bf436dbf18d30ec2
jcsamoyed <aruby5223@gmail.com>
2021-05-03 18:02:39 +0800

    add pregnancy data

--- resources/views/course/treatment/TreatmentPregnancy.vue
+++ resources/views/course/treatment/TreatmentPregnancy.vue
@@ -3,96 +3,64 @@
     :title="title"
     :type="type"
     :registry="registry"
+    :isShowSchedule="isShowSchedule"
+    :isShowSkill="isShowSkill"
+    :isShowProcess="isShowProcess"
+    :isShowFeature="isShowFeature"
+    :isShowSuitable="isShowSuitable"
+    :isShowTarget="isShowTarget"
+    :isShowPayment="isShowPayment"
+    :isShowDiscount="isShowDiscount"
     :isShowTaipei="isShowTaipei"
     :isShowTaichung="isShowTaichung"
     :isShowHsinchu="isShowHsinchu"
     :isShowTainan="isShowTainan"
     :isShowKaohsiung="isShowKaohsiung"
+    :isShowPrecaution="isShowPrecaution"
+    :isShowNotes="isShowNotes"
+    :isShowMeasure="isShowMeasure"
   >
     <template #intro>
+      <q>減緩孕期媽媽身心不適感，是提升專科知識及技能的按摩法。</q>
       <p>
-        市面上的精油品牌、芳療師頭銜琳瑯滿目已經是一股熱潮，但為什麼常常在專櫃詢問，卻得不到自己要的答案？為什麼我感覺不到精油的神奇療效？精油是大自然生命力的精華如何才能運用在自己身上？
+        美國邁阿密醫學院肢體碰觸研究中心於1982年開始，藉由科學化的研究發表於國際權威期刊，不斷提出按摩、芳療、精油對於孕婦各種身心症狀有明顯的改善與幫助的證明。
       </p>
       <p>
-        無論你是精油新手，或是接觸精油好一陣子的朋友，學習芳療的第一步至關重要，選擇適合您的課程以及正確的教學機構，將決定您是否能夠成為一個優秀的芳療師。
+        在歐美國家，懷孕婦女因治療方法的快速進步，得以早一步受益於按摩治療應用在懷孕過程之中，降低了各種懷孕的風險與身心壓力，漸漸成為許多歐美婦女懷孕過程中的必要選擇。
       </p>
       <p>
-        禾場建議您先從「 芳香療法基礎入門班
-        」開始，這是專門為從未上過認證課程的愛好者所設計的入門課程；課程的設計目的，在於芳療觀念的釐清與建立，透過國外芳療協會研究多年的系統架構學習芳療，禾場並獨創設計大量的實際操作、個案研討，強調將芳療融入生活才能夠有俯拾皆是的調配能力。
+        由於孕期媽媽在此時精神和身體上的雙重負擔，導致許多部位出現不適和酸痛，尤其在下背、頸部、肩膀、雙腿、雙腳；除了身體的不適，還伴隨情緒不穩定的症狀。
+      </p>
+      <p>
+        學習孕產婦按摩能支援孕期媽媽的情緒、安撫身心，減輕失眠，減緩負重關節的壓力，同時有助保持正確的姿勢、維持肌肉健康，幫助生產並減輕疼痛，舒緩頭痛和鼻竇不適，也能與肚子中寶寶增加互動、間接為寶寶按摩，能提早訓練感覺統合。
+      </p>
+      <p>
+        若是正你是備孕家庭的溫柔爸爸，或者照顧產孕婦的專職工作者，這都是一門促進家庭和協，增進專科知識及技巧的首選課程。
       </p>
     </template>
-    <template #schedule>
+    <template #skill>
       <div class="table_container">
-        <table class="course_table" cellpadding="15">
-          <thead>
-            <tr>
-              <th class="num"></th>
-              <th class="subject">課程內容</th>
-              <th class="system">生理檔案</th>
-              <th class="activity">課程活動</th>
-              <th class="handmade">DIY內容</th>
-            </tr>
-          </thead>
+        <table class="course_table" cellpadding="10">
           <tbody>
             <tr>
-              <td>1</td>
-              <td>
-                認識芳香療法<br />
-                芳香引領~認識精油<br />
-                精油基礎調製學
-              </td>
-              <td></td>
-              <td>
-                香氛心理測驗<br />
-                滾珠瓶體驗 <br />
-                聞香測試
-              </td>
-              <td></td>
+              <td>體力</td>
+              <td>★★★☆☆</td>
             </tr>
             <tr>
-              <td>2</td>
-              <td>
-                精油概論<br />
-                精油品質辨識<br />
-                精油基本使用方法
-              </td>
-              <td>免疫系統</td>
-              <td>
-                嗅覺大考驗<br />
-                課程目標調油<br />
-                聞香測試
-              </td>
-              <td>按摩油</td>
+              <td>節奏感</td>
+              <td>★★★☆☆</td>
             </tr>
             <tr>
-              <td>3</td>
-              <td>
-                精油萃取方式簡介<br />
-                生命之水-純露<br />
-                純露介紹：羅馬洋甘菊、橙花
-              </td>
-              <td>邊緣系統</td>
-              <td>
-                香氣抓周<br />
-                課程目標調油<br />
-                聞香測試
-              </td>
-              <td>
-                按摩油<br />
-                髮霜
-              </td>
+              <td>協調</td>
+              <td>★★★☆☆</td>
             </tr>
             <tr>
-              <td>4</td>
-              <td>
-                精油毒性與禁忌<br />
-                精油藥理動力<br />
-                嗅覺密碼<br />
-                純露介紹：兩種不同產地的保加利亞玫瑰純露、薰衣草純露
-              </td>
-              <td>神經系統</td>
-              <td>聞香測試</td>
-              <td></td>
+              <td>練習</td>
+              <td>★★★★☆</td>
+            </tr>
+            <tr>
+              <td>個案直覺感受</td>
+              <td>★★★★★</td>
             </tr>
           </tbody>
         </table>
@@ -108,36 +76,35 @@
     </template>
     <template #feature>
       <ol>
-        <li>課程架構符合國際芳療師協會NAHA、IFPA認證標準</li>
-        <li>教學生動融入DIY、體驗、回饋，講師教學深入淺出</li>
-        <li>每班有學員人數上限，確保學習品質，並與老師皆能有所交流互動</li>
         <li>
-          入門課程結業後可申請入門時數證書（禾場國際芳療學苑協助申請手續費NT$500)，為您打下未來投入芳療領域的專業基礎和條件。(全階認證課程畢業後可取得申請美國NAHA國際認證資格)
+          本課程將教授適合孕期婦女使用之芳香療法，安全劑量與用油，身心狀況調理、胎教、療程設計與乳房保健、產後(坐月子)全身護理、產後乳房按摩。
         </li>
-      </ol>
-    </template>
-    <template #suitable>
-      <ol>
-        <li>美容、SPA相關產業從業人員、相關產業主管、業者</li>
-        <li>希望取得國際芳療師認證的芳療師</li>
-        <li>醫護人員</li>
-        <li>諮商心理師、臨床心理師</li>
-        <li>想要參加正式芳療訓練精油玩家</li>
-      </ol>
-    </template>
-    <template #target>
-      <ol>
-        <li>取得NAHA美國整體芳香療法協會核可之芳療師入門證書</li>
-        <li>取得中階芳療師入學資格</li>
-        <li>具備正確、及紮實的芳療師技巧及觀念</li>
-        <li>培養芳療師獨具的敏銳觀察能力、針對他人之身心狀況解讀能力</li>
-        <li>使原本美容從業人員得以切入自然療法及整體健康治療領域</li>
+        <li>
+          使得孕婦可以降低妊娠其間的身心壓力；讓胎兒能在孕媽咪的身體之中，感受到芳療師的能量，並刺激五感早期發展。
+        </li>
+        <li>本課程手法完成後將核發修業證書。</li>
       </ol>
     </template>
     <template #payment>
       <ul>
-        <li>本課程費用優惠價 新台幣4,200元</li>
-        <li>課程共12小時，含課程費、講義、耗材</li>
+        <li>本課程費用 13,500 元，含課程耗材、講義教材、不含毛巾組</li>
+        <li>課程共 18 小時</li>
+        <li>
+          需使用芳療師專用毛巾組（有一定尺寸要求）原價 1500 ，學員優惠價格
+          500，<br />可於日後使用在所有療程課程，已經有整套就不用買喔！
+        </li>
+      </ul>
+    </template>
+    <template #discount>
+      <ul>
+        <li>禾場學員9折</li>
+        <li>兩人同行：學費優惠500元</li>
+        <li>早鳥價：學費優惠500元（需一個月前報名）</li>
+        <li>療程學苑合報優惠價：2堂以上合報85折、3堂以上合報8折</li>
+        <li>
+          Level 1 經典療程初階班 + Level 2 經典六種療程高階班合報優惠價：$60,000
+        </li>
+        <li>Level 2 經典六種療程高階班全階合報優惠價：$48,000</li>
         <li>
           關於 二階(入+中)/三階(入+中+高)/四階(入+中+高+英式按摩)
           合報優惠及其他優惠活動，請來電洽詢
@@ -277,6 +244,49 @@
         </table>
       </div>
     </template>
+    <template #precaution>
+      <ol>
+        <li>
+          療程課程內容單純為技法課程，「無」任何芳香療法內容（也不含教授如何調油或如何諮詢），若想要學習相關芳療專業，成為全方位芳療師，歡迎同時報名「<router-link
+            to="/course/aromatherapy/elementary"
+            title="芳香療法課程"
+            class="link"
+            >芳香療法課程</router-link
+          >」。
+        </li>
+        <li>
+          療程課程為技法教育，不代表任何公司或品牌之技法療程，非就業之標準。上課老師在業界擁有多年的療程經驗，但無法保證上課學員任何工作機會。療程技法注重不斷練習與經驗，上課後，學員需自行安排時間複習才能熟練與貫通。
+        </li>
+        <li>
+          芳香療法專業與療程課程「應無」性別限制，上課時可能男女混班。上課前都會提前告知該班是否有男性學員。上課過程中，講師與學員都會待在同一空間上課，敬請放心參加課程。
+        </li>
+      </ol>
+    </template>
+    <template #notes>
+      <ol>
+        <li>上課名額之保留，將以報名繳費先後為依據。</li>
+        <li>每堂課請務必簽到，以便確認您的出席。</li>
+        <li>申請「技法修業證書」需按各課程之結業規定。</li>
+        <li>
+          欲申請IFPA認證者需上Level 1 英式芳療按摩療程（含考試），Level2
+          課程為研修課程，僅上課不含考試。
+        </li>
+        <li>為維護所有參與學員之權益，本課程不提供試聽及旁聽。</li>
+        <li>
+          禾場芳療學苑所有課程恕不接受錄音、錄影、拍攝等，所有課程講義亦已登記版權。
+        </li>
+        <li>
+          任何內容如有遭到重製、散佈等權益受損之情事，禾場芳療學苑將根據智慧財產權依法追究。
+        </li>
+        <li>除不可抗力之因素外，所有課程開課後恕不接受換班。</li>
+        <li>禾場芳療學苑保留所有課程調整與變動之權利。</li>
+        <li>
+          上課期間如遇颱風等天災，依人事行政局之發佈「停班公告
+          (非停課)」比照辦理，並另擇日補課。
+        </li>
+        <li>上課前會通知加入班級群組，以利學務事項公布。</li>
+      </ol>
+    </template>
   </CourseContent>
 </template>
 
@@ -292,11 +302,22 @@ export default {
     type: "療程學苑",
     registry:
       "https://docs.google.com/forms/d/1VfaJWGZpoj2snOIwwGHamnlC_HPbwG-QLSsgtvHDIAE/viewform?edit_requested=true",
+    isShowSchedule: false,
+    isShowSkill: true,
+    isShowProcess: true,
+    isShowFeature: true,
+    isShowSuitable: false,
+    isShowTarget: false,
+    isShowPayment: true,
+    isShowDiscount: true,
     isShowTaipei: true,
     isShowTaichung: true,
     isShowHsinchu: true,
     isShowTainan: true,
     isShowKaohsiung: true,
+    isShowPrecaution: true,
+    isShowNotes: true,
+    isShowMeasure: true,
     dates: [
       "2021-03-04 (四)",
       "2021-04-01 (四)",
