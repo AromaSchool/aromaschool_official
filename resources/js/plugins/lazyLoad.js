8f391f195cef3bdb501ea3f1ad9be6d0cd6aa31e
Paul <alphagoo@g-mail.nsysu.edu.tw>
2021-05-12 21:26:52 +0800

    set error image of lazy loading

--- resources/js/plugins/lazyLoad.js
+++ resources/js/plugins/lazyLoad.js
@@ -3,7 +3,8 @@ import VueLazyload from 'vue-lazyload'
 export default {
     install(Vue) {
         Vue.use(VueLazyload, {
-            loading: require('@/image/loading.svg')
+            loading: require('@/image/loading.svg'),
+            error: require('@/image/blog/blog_default.svg'),
         })
     }
 }
