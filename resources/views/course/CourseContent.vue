df402b9808cd3c716c2299fddef54125b8d646a7
jcsamoyed <aruby5223@gmail.com>
2021-05-13 18:59:07 +0800

    update course measure data

--- resources/views/course/CourseContent.vue
+++ resources/views/course/CourseContent.vue
@@ -34,13 +34,17 @@
         <p class="note">※課表內容以學苑核發之實際課表為主</p>
       </div>
       <div class="course_box skill" v-if="isShowSkill == true">
-        <h4 class="course_title"><i class="fas fa-hand-sparkles"></i>手法屬性</h4>
+        <h4 class="course_title">
+          <i class="fas fa-hand-sparkles"></i>手法屬性
+        </h4>
         <div class="skill_block">
           <slot name="skill"></slot>
         </div>
       </div>
       <div class="course_box process" v-if="isShowProcess == true">
-        <h4 class="course_title"><i class="fas fa-swatchbook"></i>國際認證進修流程</h4>
+        <h4 class="course_title">
+          <i class="fas fa-swatchbook"></i>國際認證進修流程
+        </h4>
         <slot name="process"></slot>
         <div class="btn_right">
           <a
@@ -58,7 +62,9 @@
         <slot name="feature"></slot>
       </div>
       <div class="course_box suitable" v-if="isShowSuitable == true">
-        <h4 class="course_title"><i class="fas fa-user-friends"></i>適合對象</h4>
+        <h4 class="course_title">
+          <i class="fas fa-user-friends"></i>適合對象
+        </h4>
         <slot name="suitable"></slot>
       </div>
       <div class="course_box target" v-if="isShowTarget == true">
@@ -66,7 +72,9 @@
         <slot name="target"></slot>
       </div>
       <div class="course_box payment" v-if="isShowPayment == true">
-        <h4 class="course_title"><i class="fas fa-comments-dollar"></i>課程費用說明</h4>
+        <h4 class="course_title">
+          <i class="fas fa-comments-dollar"></i>課程費用說明
+        </h4>
         <slot name="payment"></slot>
       </div>
       <div class="course_box discount" v-if="isShowDiscount == true">
@@ -187,7 +195,9 @@
         </div>
       </div>
       <div class="course_box precaution" v-if="isShowPrecaution == true">
-        <h4 class="course_title"><i class="fas fa-exclamation-circle"></i>報名注意事項</h4>
+        <h4 class="course_title">
+          <i class="fas fa-exclamation-circle"></i>報名注意事項
+        </h4>
         <slot name="precaution"></slot>
       </div>
       <div class="course_box notes" v-if="isShowNotes == true">
@@ -201,11 +211,18 @@
         <div class="measure_box">
           <p>現在全球最關心的就是 COVID-19，本學苑與政府的資訊也是同步的。</p>
           <p>
-            目前政府尚「未發佈」不上班上課的資訊，請各位一定要多注意，但切勿恐慌。（保持心情愉快，也是增強免疫力的一種方式哦）學苑目前並未因疫情而影響到開課，各位都非常期待來學芳療保健的專業（因這次的疫情，各位更重視自我保健方式了），本學苑於上課前都會提醒學員上課時戴上口罩、進教室前先進行雙手消毒、量額溫，教室也會定時消毒清潔，並全天用純天然精油擴香，且每班都為小班制。
+            政府目前「尚未發佈」停班停課的資訊，請各位一定要多注意，但切勿恐慌。（保持心情正向愉快，也是增強免疫力的一種方式哦）
+            學苑目前並未因疫情而影響到開課，各位都非常期待來學芳療保健的專業。（因這次的疫情，各位更重視自我保健方式了呢）
           </p>
           <p>
-            提醒大家平時還是要勤洗手、多喝水、出門戴口罩、維持良好的生活作習，身體不舒服一定要看醫生。回到家時先將衣物、物品消毒過一遍，先保護好自已。<br />
-            相信我們大家都能夠平安度過這次的疫情的唷。
+            本學苑教室採取定時消毒清潔，課程每班都為小班制，上課學員採「間隔坐」，學苑全天侯用純天然精油進行擴香。
+            且於上課前都會提醒學員、講師於上課時"全程"戴口罩、自備水杯、進教室前先進行雙手酒精消毒、量額溫。
+          </p>
+          <p>
+            另外也提醒大家平時要勤洗手、多喝水、出門戴口罩、維持良好的生活作息。
+            回到家時先將衣物、物品消毒過一遍，先保護好自已。
+            身體確認有不適請先向相關醫療單位諮詢。
+            相信台灣、相信全世界，相信我們大家都能夠平安度過這次的疫情的唷。
           </p>
           <p>祝 順心健康。</p>
           <div class="btn_right">
